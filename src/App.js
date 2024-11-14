332293cef858a2b5373896536f88be48239c089b
Milan Pjevic <milanpjevic@icloud.com>
2024-11-14 17:08:28 +0100

    State Variable - useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 /** @format */
 
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -7,22 +9,24 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step === 1) return;
+    setStep(step - 1);
   }
 
   function handleNext() {
-    alert("Next");
+    if (step === 3) return;
+    setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
