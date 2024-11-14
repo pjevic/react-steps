2992d1198658d6f75df1f5a1431871e6b8d356b0
Milan Pjevic <milanpjevic@icloud.com>
2024-11-14 18:04:48 +0100

    Updating State Based on Current State

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ export default function App() {
 
   function handlePrevious() {
     if (step === 1) return;
-    setStep(step - 1);
+    setStep((s) => s - 1);
   }
 
   function handleNext() {
     if (step === 3) return;
-    setStep(step + 1);
+    setStep((s) => s + 1);
   }
 
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)} className="close">
+      <button onClick={() => setIsOpen((s) => !s)} className="close">
         &times;
       </button>
 
